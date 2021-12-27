0ca1ec59468c8ddb702ffba5837f9d2289c3104d
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-12-27 18:52:52 +0330

    add images

---
+++ src/assets/images/foods/index.ts
@@ -0,0 +1,27 @@
+const FOOD1 = require('./1.png');
+const FOOD2 = require('./2.png');
+const FOOD3 = require('./3.png');
+const FOOD4 = require('./4.png');
+const FOOD5 = require('./5.png');
+const FOOD6 = require('./6.png');
+const FOOD7 = require('./7.png');
+const FOOD8 = require('./8.png');
+const FOOD9 = require('./9.png');
+const FOOD10 = require('./10.png');
+const FOOD11 = require('./11.png');
+const FOOD12 = require('./12.png');
+
+export {
+  FOOD1,
+  FOOD2,
+  FOOD3,
+  FOOD4,
+  FOOD5,
+  FOOD6,
+  FOOD7,
+  FOOD8,
+  FOOD9,
+  FOOD10,
+  FOOD11,
+  FOOD12,
+};
---
+++ src/assets/images/index.ts
@@ -0,0 +1,19 @@
+const LOGO = require('./logo.png');
+const GRILLED_BEEF = require('./grilled-beef-tomato-skewers_144627-36524.jpg');
+const HEALTHY_ASIAN = require('./delicious-healthy-asian-food-black-textured-background-with-copy-space-min.jpg');
+const ROASTED_STEAK = require('./roasted-pork-steak-dark-wooden-surface-min.jpg');
+const ITALIAN_SPAGHETTI = require('./italian-spaghetti-with-tomato-sauce-pan-min.jpg');
+const GROENTE_RAMEN = require('./Groente-ramen.jpg');
+const TABLE = require('./book-table.png');
+const INITIAL = require('./initial.jpg');
+
+export {
+  LOGO,
+  GRILLED_BEEF,
+  HEALTHY_ASIAN,
+  ROASTED_STEAK,
+  ITALIAN_SPAGHETTI,
+  GROENTE_RAMEN,
+  TABLE,
+  INITIAL,
+};
