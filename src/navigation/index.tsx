80e86a4af47f4a443810aa29229ab3068d93a71a
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-12-27 19:05:34 +0330

    change navigation

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {MainStackParamList} from '../constants/TypeScript';
 import {
   InitialScreen,
   HomeScreen,
   LoginScreen,
   RegisterScreen,
 } from '../screens';
-import {Header} from 'react-native-elements';
-import {colors} from '../constants/colors';
-import {RFValue} from 'react-native-responsive-fontsize';
+import {RootStackParamList} from '../constants/TypeScript';
 
-const MainStack = createNativeStackNavigator<MainStackParamList>();
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Screens() {
   return (
     <NavigationContainer>
-      <MainStack.Navigator
-        initialRouteName="InitialScreen"
+      <RootStack.Navigator
         screenOptions={{
           headerShown: false,
         }}>
-        <MainStack.Screen name="InitialScreen" component={InitialScreen} />
-        <MainStack.Screen name="LoginScreen" component={LoginScreen} />
-        <MainStack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <MainStack.Screen name="HomeScreen" component={HomeScreen} />
-      </MainStack.Navigator>
+        <RootStack.Screen name="InitialScreen" component={InitialScreen} />
+        <RootStack.Screen name="LoginScreen" component={LoginScreen} />
+        <RootStack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <RootStack.Screen name="HomeScreen" component={HomeScreen} />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
