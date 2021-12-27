63e9a5b8397e033eddcc460502179c955203341e
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-12-27 19:01:32 +0330

    change and add types

--- src/constants/TypeScript.ts
+++ src/constants/TypeScript.ts
@@ -1,7 +1,11 @@
+import {ImageSourcePropType} from 'react-native';
+
 export interface Colors {
   WHITE: string;
   BLACK: string;
   DARK: string;
+  DARK_2: string;
+  DARKEN: string;
   LIGHT: string;
   GREEN: string;
   BLUE: string;
@@ -9,20 +13,60 @@ export interface Colors {
   YELLOW: string;
   ORANGE: string;
   GREY: string;
+  GREY_2: string;
+  GREY_3: string;
+  GREY_4: string;
   LIGHT_ORANGE: string;
   LIGHT_GREY: string;
   LIGHT_BLUE: string;
+  LIGHT_SHADOW: string;
+  DARK_SHADOW: string;
+  DARKEN_SHADOW: string;
+  NONE: string;
 }
 
-export type RouteParamType =
-  | 'InitialScreen'
-  | 'HomeScreen'
-  | 'LoginScreen'
-  | 'RegisterScreen';
-
-export type MainStackParamList = {
+export type RootStackParamList = {
   InitialScreen: undefined;
   HomeScreen: undefined;
   LoginScreen: undefined;
   RegisterScreen: undefined;
 };
+
+export interface Food {
+  _id: string;
+  name: string;
+  type: string;
+  price: string;
+  images: ImageSourcePropType[];
+  category: string;
+  compositions: string[];
+  desc: string;
+  rating: number;
+  calories: number;
+  weight: number;
+}
+
+export interface Restaraunt {
+  _id: string;
+  name: string;
+  category: string;
+  logo: string;
+  address: string;
+  tel: string;
+  position: {lag: string; lat: string};
+  rating: number;
+  distance?: number;
+  isOpen: boolean;
+  verified: boolean;
+  slogan: string;
+  workHours: {from: string; to: string};
+  managerName: string;
+  chef: string;
+  visitThisMonth: number;
+  reviews: string[];
+  images: string[];
+  tags: string[];
+  socials: string[];
+  website: string;
+  menu: Food[];
+}
