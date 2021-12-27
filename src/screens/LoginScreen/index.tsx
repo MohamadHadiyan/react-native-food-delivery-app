43043a1682c6c8dffdcd1e4c8010c2f78465f088
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-12-27 19:15:49 +0330

    change LoginScreen

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,24 +1,139 @@
-import React, {useMemo} from 'react';
-import {View, Text} from 'react-native';
-import {
-  NavigationProp,
-  ParamListBase,
-  RouteProp,
-} from '@react-navigation/native';
+import React, {useMemo, useState} from 'react';
+import {Pressable, ScrollView, Switch, View} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import LinearGradient from 'react-native-linear-gradient';
+import {Button, Image, Input, Text} from 'react-native-elements';
+import {RootStackParamList} from '../../constants/TypeScript';
+import {colors} from '../../constants/colors';
 import createStyles from './styles';
-import {MainStackParamList} from '../../constants/TypeScript';
+import {LOGO} from '../../assets/images';
 
-interface IProps {
-  navigation: NavigationProp<ParamListBase>;
-  route: RouteProp<MainStackParamList>;
-}
+type Props = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
 
-export default function LoginScreen({navigation, route}: IProps) {
+export default function LoginScreen({navigation}: Props) {
   const styles = useMemo(() => createStyles(), []);
+  const [account, setAccount] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}>Login Screen</Text>
+    <View style={[styles.body, styles.center]}>
+      <ScrollView>
+        <View style={styles.center}>
+          <View style={styles.accountBox}>
+            <Image source={LOGO} style={styles.logo} />
+            <Text h3 style={styles.title}>
+              Welcome Back
+            </Text>
+            <View style={styles.inputContainer}>
+              <Input
+                leftIcon={{
+                  name: 'user',
+                  type: 'font-awesome',
+                  color: colors.LIGHT_GREY,
+                }}
+                value={account}
+                onChangeText={text => setAccount(text)}
+                placeholderTextColor={colors.GREY}
+                inputStyle={styles.input}
+                placeholder="Phone number or email address"
+                keyboardAppearance="dark"
+                autoCompleteType
+                errorMessage=""
+              />
+              <Input
+                leftIcon={{
+                  name: 'lock',
+                  type: 'material-icons',
+                  color: colors.LIGHT_GREY,
+                }}
+                rightIcon={{
+                  name: showPassword ? 'visibility-off' : 'visibility',
+                  type: 'material-icons',
+                  color: colors.LIGHT_GREY,
+                  onPress: () => setShowPassword(!showPassword),
+                }}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                placeholderTextColor={colors.GREY}
+                inputStyle={styles.input}
+                placeholder="Password"
+                textContentType="password"
+                keyboardAppearance="dark"
+                secureTextEntry
+                autoCompleteType
+                errorMessage=""
+              />
+
+              <View style={styles.switchBox}>
+                <View style={styles.rememberBox}>
+                  <Switch
+                    trackColor={{true: colors.ORANGE, false: colors.LIGHT_GREY}}
+                    thumbColor={rememberMe ? colors.WHITE : colors.GREY}
+                    value={rememberMe}
+                    onValueChange={value => setRememberMe(value)}
+                  />
+                  <Text style={[styles.smallText, styles.marginLeft_5]}>
+                    Remember me
+                  </Text>
+                </View>
+                <Text style={styles.smallText}>Forgot Password?</Text>
+              </View>
+            </View>
+          </View>
+
+          <Button
+            onPress={() => navigation.navigate('HomeScreen')}
+            title="Sign in"
+            titleStyle={styles.textButton}
+            containerStyle={styles.containerButton}
+            buttonStyle={styles.button}
+            style={styles.fullWidth}
+            ViewComponent={LinearGradient}
+            linearGradientProps={{
+              colors: [colors.ORANGE, colors.LIGHT_ORANGE],
+            }}
+          />
+          <Text style={styles.text}>Or</Text>
+
+          <View style={styles.socialBox}>
+            <View style={styles.socialButtonBox}>
+              <Button
+                title="Google+"
+                titleStyle={styles.socialTitle}
+                icon={{
+                  name: 'google',
+                  type: 'font-awesome',
+                  color: colors.RED,
+                }}
+                buttonStyle={styles.socialButton}
+                containerStyle={styles.socialBtnContainer}
+              />
+              <Button
+                title="Facebook"
+                titleStyle={[styles.socialTitle, styles.marginLeft_5]}
+                icon={{
+                  name: 'facebook',
+                  type: 'font-awesome',
+                  color: colors.BLUE,
+                }}
+                buttonStyle={styles.socialButton}
+                containerStyle={styles.socialBtnContainer}
+              />
+            </View>
+
+            <View style={styles.signupBox}>
+              <Text style={styles.smallText}>Don't have an account?</Text>
+              <Pressable onPress={() => navigation.navigate('RegisterScreen')}>
+                <Text style={[styles.smallText, styles.signupLink]}>
+                  Sign up
+                </Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- src/screens/LoginScreen/styles.ts
+++ src/screens/LoginScreen/styles.ts
@@ -6,13 +6,119 @@ const createStyles = () =>
   StyleSheet.create({
     body: {
       flex: 1,
+      backgroundColor: colors.DARK,
+    },
+    center: {
       justifyContent: 'center',
       alignItems: 'center',
     },
-    text: {
+    logo: {
+      width: RFValue(120),
+      height: RFValue(120),
+      marginTop: RFValue(30),
+      marginBottom: RFValue(10),
+    },
+    title: {
       fontSize: RFValue(25),
-      fontWeight: '300',
-      color: colors.BLACK,
+      fontWeight: '600',
+      color: colors.ORANGE,
+      fontFamily: 'Roboto',
+    },
+    text: {
+      fontSize: RFValue(18),
+      color: colors.LIGHT_GREY,
+    },
+    smallText: {
+      fontSize: RFValue(12),
+      color: colors.LIGHT_GREY,
+    },
+    accountBox: {
+      width: '100%',
+      paddingHorizontal: RFValue(20),
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    inputContainer: {
+      width: '100%',
+      padding: 10,
+      backgroundColor: colors.DARKEN,
+      borderRadius: 12,
+      elevation: 10,
+      shadowColor: colors.BLACK,
+      marginVertical: RFValue(30),
+      marginHorizontal: RFValue(20),
+    },
+    input: {
+      color: colors.LIGHT,
+    },
+    switchBox: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      marginTop: 10,
+      paddingHorizontal: 10,
+    },
+    rememberBox: {
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    marginLeft_5: {
+      marginLeft: 6,
+    },
+    fullWidth: {
+      width: '100%',
+    },
+    containerButton: {
+      width: '100%',
+      height: RFValue(75),
+      alignItems: 'center',
+      paddingHorizontal: RFValue(20),
+    },
+    button: {
+      height: RFValue(50),
+      borderRadius: 5,
+      elevation: 10,
+      shadowColor: colors.LIGHT_ORANGE,
+    },
+    textButton: {
+      textTransform: 'uppercase',
+      fontSize: RFValue(18),
+      color: colors.WHITE,
+    },
+    socialBox: {
+      width: '100%',
+      paddingHorizontal: RFValue(20),
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: RFValue(20),
+    },
+    socialTitle: {
+      color: colors.LIGHT,
+      fontSize: RFValue(18),
+      paddingVertical: 6,
+      paddingStart: 10,
+    },
+    socialButtonBox: {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+    },
+    socialButton: {
+      backgroundColor: colors.DARKEN,
+      justifyContent: 'flex-start',
+    },
+    socialBtnContainer: {
+      marginBottom: 10,
+      elevation: 6,
+      shadowColor: colors.BLACK,
+    },
+    signupBox: {
+      flexDirection: 'row',
+      marginVertical: RFValue(20),
+    },
+    signupLink: {
+      color: colors.LIGHT_ORANGE,
+      marginLeft: 10,
     },
   });
 
