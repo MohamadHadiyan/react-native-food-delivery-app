46d90888c17d59a02d643498f98606eec689ed51
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-12-27 19:13:34 +0330

    complete InitialScreen

--- src/screens/InitialScreen/index.tsx
+++ src/screens/InitialScreen/index.tsx
@@ -1,18 +1,72 @@
 import React, {useMemo} from 'react';
-import {View, Text} from 'react-native';
-import {NavigationProp, ParamListBase} from '@react-navigation/native';
+import {View, Text, ImageBackground} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import LinearGradient from 'react-native-linear-gradient';
+import {Button, Image} from 'react-native-elements';
+import Swiper from 'react-native-swiper';
+import {RootStackParamList} from '../../constants/TypeScript';
+import {colors} from '../../constants/colors';
 import createStyles from './styles';
+import {
+  GRILLED_BEEF,
+  HEALTHY_ASIAN,
+  INITIAL,
+  LOGO,
+  ROASTED_STEAK,
+} from '../../assets/images';
 
-interface IProps {
-  navigation: NavigationProp<ParamListBase>;
-}
+type Props = NativeStackScreenProps<RootStackParamList, 'InitialScreen'>;
 
-export default function InitialScreen({navigation}: IProps) {
+export default function InitialScreen({navigation}: Props) {
   const styles = useMemo(() => createStyles(), []);
 
+  const iconOption = {
+    name: 'east',
+    type: 'material-icons',
+    size: 28,
+    color: colors.WHITE,
+  };
+
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}>Initial Screen</Text>
-    </View>
+    <ImageBackground
+      source={INITIAL}
+      style={[styles.body, styles.center]}
+      blurRadius={10}>
+      <LinearGradient
+        colors={['#13131ace', '#13131a58']}
+        style={styles.gradient}
+      />
+      <Image source={LOGO} style={styles.logo} />
+      <Swiper
+        autoplayTimeout={4}
+        dotColor={colors.GREY_4}
+        activeDotColor={colors.ORANGE}
+        autoplay>
+        <View style={styles.slide}>
+          <Image source={ROASTED_STEAK} style={styles.image} />
+          <Text style={styles.title}>Enjoy Instant Delivery and Payment</Text>
+        </View>
+        <View style={styles.slide}>
+          <Image source={GRILLED_BEEF} style={styles.image} />
+          <Text style={styles.title}>Quick delivery at your Doorstep</Text>
+        </View>
+        <View style={styles.slide}>
+          <Image source={HEALTHY_ASIAN} style={styles.image} />
+          <Text style={styles.title}>All Your Faourite Dishes</Text>
+        </View>
+      </Swiper>
+      <View style={styles.center}>
+        <Button
+          onPress={() => navigation.navigate('LoginScreen')}
+          icon={iconOption}
+          containerStyle={[styles.containerButton, styles.center]}
+          buttonStyle={styles.button}
+          ViewComponent={LinearGradient}
+          linearGradientProps={{
+            colors: [colors.ORANGE, colors.LIGHT_ORANGE],
+          }}
+        />
+      </View>
+    </ImageBackground>
   );
 }
--- src/screens/InitialScreen/styles.ts
+++ src/screens/InitialScreen/styles.ts
@@ -6,13 +6,51 @@ const createStyles = () =>
   StyleSheet.create({
     body: {
       flex: 1,
+      backgroundColor: colors.DARK,
+    },
+    center: {
       justifyContent: 'center',
       alignItems: 'center',
     },
-    text: {
+    gradient: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      bottom: 0,
+      right: 0,
+    },
+    logo: {
+      width: RFValue(150),
+      height: RFValue(150),
+      marginTop: RFValue(30),
+      marginBottom: 10,
+    },
+    title: {
       fontSize: RFValue(25),
-      fontWeight: '300',
-      color: colors.BLACK,
+      fontWeight: '600',
+      color: colors.LIGHT,
+      textAlign: 'center',
+      marginTop: RFValue(20),
+    },
+    slide: {
+      paddingHorizontal: 10,
+    },
+    image: {
+      width: '100%',
+      height: RFValue(300),
+      borderRadius: 20,
+    },
+    containerButton: {
+      width: RFValue(100),
+    },
+    button: {
+      height: RFValue(60),
+      width: RFValue(60),
+      borderRadius: RFValue(30),
+      elevation: 10,
+      shadowColor: colors.BLACK,
+      marginTop: 10,
+      marginBottom: 20,
     },
   });
 
